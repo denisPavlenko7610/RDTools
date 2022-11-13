abd1964113e9ccb90c25e658cac7ac1fb6e4d95d
Denis Pavlenko <denispavlenko7610@gmail.com>
2022-11-13 12:25:28 +0200

    add with vector extensions

--- Runtime/Extensions/VectorExtensions.cs
+++ Runtime/Extensions/VectorExtensions.cs
@@ -4,53 +4,122 @@ namespace RDTools.Extensions
 {
     public static class VectorExtensions
     {
-        /// <summary>
-        /// Return a copy of this vector with an altered x and/or y and/or z
-        /// </summary>
-        public static Vector3 With(this Vector3 original, float? x = null, float? y = null, float? z = null)
-        {
-            return new Vector3(x ?? original.x, y ?? original.y, z ?? original.z);
-        }
+        /*
+         *	Vector2 Utils
+         */
 
-        /// <summary>
-        /// Return a copy of this vector with an altered x and/or y
-        /// </summary>
-        public static Vector2 With(this Vector2 original, float? x = null, float? y = null) =>
-            new Vector2(x ?? original.x, y ?? original.y);
+        public static Vector2 WithX(this Vector2 vec, float x) => new(x, vec.y);
 
-        /// <summary>
-        /// Return this vector with only its x and y
-        /// </summary>
-        public static Vector2 ToVector2(this Vector3 v) => new Vector2(v.x, v.y);
+        public static Vector2 WithY(this Vector2 vec, float y) => new(vec.x, y);
 
-        /// <summary>
-        /// Return a copy of this vector with an altered x
-        /// </summary>
-        public static Vector2 ChangeX(this Vector2 v, float x) => new Vector2(x, v.y);
 
-        /// <summary>
-        /// Return a copy of this vector with an altered y component
-        /// </summary>
-        public static Vector2 ChangeY(this Vector2 v, float y) => new Vector2(v.x, y);
+        public static Vector2 AddX(this Vector2 vec, float x) => new(vec.x + x, vec.y);
 
-        /// <summary>
-        /// Return a copy of this vector with an altered x
-        /// </summary>
-        public static Vector3 ChangeX(this Vector3 v, float x) => new Vector3(x, v.y, v.z);
+        public static Vector2 AddY(this Vector2 vec, float y) => new(vec.x, vec.y + y);
 
-        /// <summary>
-        /// Return a copy of this vector with an altered y
-        /// </summary>
-        public static Vector3 ChangeY(this Vector3 v, float y) => new Vector3(v.x, y, v.z);
 
-        /// <summary>
-        /// Return a copy of this vector with an altered z
-        /// </summary>
-        public static Vector3 ChangeZ(this Vector3 v, float z) => new Vector3(v.x, v.y, z);
+        public static Vector2 InvertX(this Vector2 vec) => new(-vec.x, vec.y);
 
-        /// <summary>
-        /// Return a Vector3 with this vector's components as well as the supplied z component
-        /// </summary>
-        public static Vector3 ChangeZ(this Vector2 v, float z) => new Vector3(v.x, v.y, z);
+        public static Vector2 InvertY(this Vector2 vec) => new(vec.x, -vec.y);
+
+        public static Vector2 Invert(this Vector2 vec) => new(-vec.x, -vec.y);
+
+        /*
+         *	Vector3 Utils
+         */
+
+        public static Vector3 WithX(this Vector3 vec, float x) => new(x, vec.y, vec.z);
+
+        public static Vector3 WithY(this Vector3 vec, float y) => new(vec.x, y, vec.z);
+
+        public static Vector3 WithZ(this Vector3 vec, float z) => new(vec.x, vec.y, z);
+
+
+        public static Vector3 AddX(this Vector3 vec, float x) => new(vec.x + x, vec.y, vec.z);
+
+        public static Vector3 AddY(this Vector3 vec, float y) => new(vec.x, vec.y + y, vec.z);
+
+        public static Vector3 AddZ(this Vector3 vec, float z) => new(vec.x, vec.y, vec.z + z);
+
+
+        public static Vector3 InvertX(this Vector3 vec) => new(-vec.x, vec.y, vec.z);
+
+        public static Vector3 InvertY(this Vector3 vec) => new(vec.x, -vec.y, vec.z);
+
+        public static Vector3 InvertZ(this Vector3 vec) => new(vec.x, vec.y, -vec.z);
+
+        public static Vector3 Invert(this Vector3 vec) => new(-vec.x, -vec.y, -vec.z);
+
+        /*
+         *	Vector2Int Utils
+         */
+
+        public static Vector2Int WithX(this Vector2Int vec, int x) => new(x, vec.y);
+
+        public static Vector2Int WithY(this Vector2Int vec, int y) => new(vec.x, y);
+
+
+        public static Vector2Int AddX(this Vector2Int vec, int x) => new(vec.x + x, vec.y);
+
+        public static Vector2Int AddY(this Vector2Int vec, int y) => new(vec.x, vec.y + y);
+
+
+        public static Vector2Int InvertX(this Vector2Int vec) => new(-vec.x, vec.y);
+
+        public static Vector2Int InvertY(this Vector2Int vec) => new(vec.x, -vec.y);
+
+        public static Vector2Int Invert(this Vector2Int vec) => new(-vec.x, -vec.y);
+
+        /*
+         *	Vector3Int Utils
+         */
+
+        public static Vector3Int WithX(this Vector3Int vec, int x) => new(x, vec.y, vec.z);
+
+        public static Vector3Int WithY(this Vector3Int vec, int y) => new(vec.x, y, vec.z);
+
+        public static Vector3Int WithZ(this Vector3Int vec, int z) => new(vec.x, vec.y, z);
+
+
+        public static Vector3Int AddX(this Vector3Int vec, int x) => new(vec.x + x, vec.y, vec.z);
+
+        public static Vector3Int AddY(this Vector3Int vec, int y) => new(vec.x, vec.y + y, vec.z);
+
+        public static Vector3Int AddZ(this Vector3Int vec, int z) => new(vec.x, vec.y, vec.z + z);
+
+
+        public static Vector3Int InvertX(this Vector3Int vec) => new(-vec.x, vec.y, vec.z);
+
+        public static Vector3Int InvertY(this Vector3Int vec) => new(vec.x, -vec.y, vec.z);
+
+        public static Vector3Int InvertZ(this Vector3Int vec) => new(vec.x, vec.y, -vec.z);
+
+        public static Vector3Int Invert(this Vector3Int vec) => new(-vec.x, -vec.y, -vec.z);
+
+        /*
+         *  VectorInt -> Vector
+         */
+
+        public static Vector3 ToVector3(this Vector2Int v) => new(v.x, v.y);
+
+        public static Vector3 ToVector3(this Vector3Int v) => new(v.x, v.y, v.z);
+
+
+        public static Vector2 ToVector2(this Vector2Int v) => new(v.x, v.y);
+
+        public static Vector2 ToVector2(this Vector3Int v) => new(v.x, v.y);
+
+        /*
+         *  Vector -> VectorInt
+         */
+
+        public static Vector3Int ToVector3Int(this Vector3 v) => new((int)v.x, (int)v.y, (int)v.z);
+
+        public static Vector3Int ToVector3Int(this Vector2 v) => new((int)v.x, (int)v.y, 0);
+
+
+        public static Vector2Int ToVector2Int(this Vector3 v) => new((int)v.x, (int)v.y);
+
+        public static Vector2Int ToVector2Int(this Vector2 v) => new((int)v.x, (int)v.y);
     }
 }
