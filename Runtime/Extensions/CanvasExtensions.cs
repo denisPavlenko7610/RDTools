76e621813aaa8d73970f2ad366b95bac2952d888
Denis Pavlenko <denispavlenko7610new@gmail.com>
2023-01-02 12:59:39 +0200

    add canvas extensions

---
+++ Runtime/Extensions/CanvasExtensions.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+ 
+using namespace RDTools.Extensions
+{
+    public static class CanvasExtensions {
+        public static Vector2 WorldToCanvas(this Canvas canvas, Vector3 worldPoint, Camera camera) {
+            Vector2 viewportPos = camera.WorldToViewportPoint(worldPoint);
+            RectTransform canvasRect = (RectTransform)canvas.transform;
+ 
+            return new Vector2((viewportPos.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f),
+                (viewportPos.y * canvasRect.sizeDelta.y) - (canvasRect.sizeDelta.y * 0.5f));
+        }
+    }  
+}
