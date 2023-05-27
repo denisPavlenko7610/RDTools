c85db47e01a2ff17d34cbf32cb7984cfd7a8b1f4
denis <denispavlenko7610new@gmail.com>
2023-05-27 13:56:43 +0300

    del

--- Runtime/Extensions/StringExtensions.cs
+++ Runtime/Extensions/StringExtensions.cs
@@ -6,7 +6,5 @@ namespace RDTools.Extensions
     public static class StringExtensions
     {
         public static string ToUpperFirstChar(this string word) => char.ToUpper(word[0]) + word[1..];
-
-        public static int GetRandomObject(this List<string> words) => Random.Range(0, words.Count);
     }
 }
